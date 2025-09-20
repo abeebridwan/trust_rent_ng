15d2b2260e24380214e4f12919741d632a239aca
abeeb ridwan <olumideuae@gmail.com>
2025-09-20 08:11:52 +0100

    About page: why section mobile and desktop completed

--- src/app/(legal)/about-us/page.tsx
+++ src/app/(legal)/about-us/page.tsx
@@ -1,9 +1,11 @@
 import Mission from "@/components/about-us/Mission";
+import WhyChooseSection from "@/components/homepage/WhyChooseSection";
 
 export default function AboutUsPage() {
   return (
     <>
       <Mission />
+      <WhyChooseSection />
     </>
   );
 }
