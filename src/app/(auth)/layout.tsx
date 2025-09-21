4b0e77038dbed68455dc002819629b3ec4a3c352
abeeb ridwan <olumideuae@gmail.com>
2025-09-21 13:12:18 +0100

    auth login: mobile completed

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,23 @@
 import Image from "next/image";
 import homesettingAuth from "@/assets/Images/homesetting-auth.jpg";
+import homesettingAuthMobile from "@/assets/Images/homesetting-auth-mobile.jpg";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container-nopad mx-auto px-4">
-      <div className="relative w-full min-h-[982px] hidden md:block overflow-hidden">
+      <div className="relative w-full min-h-[932px] block sm:hidden overflow-hidden">
+        <Image 
+          src={homesettingAuthMobile} 
+          alt="Home Setting" 
+          fill 
+          className="object-cover" 
+          priority
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          {children}
+        </div>
+      </div>
+      <div className="relative w-full min-h-[982px] hidden sm:block overflow-hidden">
         <Image 
           src={homesettingAuth} 
           alt="Home Setting" 
@@ -12,7 +25,6 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
           className="object-cover" 
           priority
         />
-        {/* Centered container for children */}
         <div className="absolute inset-0 flex items-center justify-center">
           {children}
         </div>
--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <Card className="z-10 w-full max-w-md rounded-none flex flex-col justify-center items-center">
+    <Card className="z-10 w-full mx-4 sm:mx-0 max-w-sm sm:max-w-md rounded-none flex flex-col justify-center items-center">
       <CardHeader className="flex flex-col justify-center items-center">
         <div className="flex items-center overflow-hidden">
           {/* Desktop Logo */}
@@ -32,7 +32,7 @@ export default function LoginPage() {
               alt="Vetarent Logo" 
               width={150} 
               height={50} 
-              className="hidden md:block " 
+              className="hidden sm:block " 
             />
           </Link>
           {/* Mobile Logo */}
@@ -40,15 +40,15 @@ export default function LoginPage() {
             <Image 
               src={Logo1} 
               alt="Vetarent Logo" 
-              width={111.37} 
-              height={37} 
-              className="block md:hidden" 
+              width={172} 
+              height={57} 
+              className="block sm:hidden" 
             />
           </Link>
         </div>
-        <CardTitle className="text-2xl font-semibold !-mt-1 tracking-widest">LOGIN</CardTitle>
+        <CardTitle className="text-lg sm:text-2xl font-semibold !-mt-1 tracking-widest">LOGIN</CardTitle>
       </CardHeader>
-      <CardContent className="w-full">
+      <CardContent className="w-full p-4 sm:p-6">
         <div className="grid gap-4">
           <div className="grid gap-2">
             <Input
@@ -87,41 +87,41 @@ export default function LoginPage() {
                 />
                 <label
                   htmlFor="remember"
-                  className="text-sm text-gray-400 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  className="text-xs sm:text-sm text-gray-400 font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                 >
                   Remember Me
                 </label>
               </div>
-              <Link href="/forgot-password" className="inline-block text-sm font-bold text-vetarent-orange hover:underline">
+              <Link href="/forgot-password" className="inline-block text-xs sm:text-sm font-bold text-vetarent-orange hover:underline">
                 Forgot Password?
               </Link>
             </div>
           </div>
             <Button
-            className="w-full h-14 bg-[#0D47A1] text-white font-semibold text-base rounded-none shadow-[inset_4px_8px_8px_rgba(255,255,255,0.25),inset_-4px_-8px_8px_rgba(0,0,0,0.25)] hover:bg-[#1565C0] transition-all duration-200"
+            className="w-full h-12 bg-[#0D47A1] mt-4 sm:mt-4 text-white font-semibold text-sm sm:text-base rounded-none shadow-[inset_4px_8px_8px_rgba(255,255,255,0.25),inset_-4px_-8px_8px_rgba(0,0,0,0.25)] hover:bg-[#1565C0] transition-all duration-200"
           >
             Login
           </Button>
 
           <div className="flex items-center gap-2 mt-2">
             <hr className="flex-grow border-gray-200" />
-            <span className="text-gray-500 text-lg">OR</span>
+            <span className="text-gray-500 text-sm sm:text-lg">OR</span>
             <hr className="flex-grow border-gray-200" />
           </div>
 
-          <Button variant="outline" className="w-full h-14 text-base font-semibold flex items-center justify-center gap-2 py-6 rounded-none border-[1px] border-gray-400 hover:border-vetarent-orange">
+          <Button variant="outline" className="w-full h-12 text-sm sm:text-base font-semibold flex items-center justify-center gap-2 py-6 rounded-none border-[1px] border-gray-400 hover:border-vetarent-orange">
             <Image src={GoogleLogo} alt="Google" width={20} height={20} />
             Login with Google
           </Button>
-          <Button variant="outline" className="w-full h-14 text-base font-semibold flex items-center justify-center gap-2 py-6 rounded-none border-[1px] border-gray-400 hover:border-vetarent-orange">
+          <Button variant="outline" className="w-full h-12 text-sm sm:text-base font-semibold flex items-center justify-center gap-2 py-6 rounded-none border-[1px] border-gray-400 hover:border-vetarent-orange">
             <Image src={AppleLogo} alt="Apple" width={20} height={20} />
             Login with Apple
           </Button>
         </div>
-        <hr className="my-4 text-gray-200 mt-6" />
-        <div className="mt-6 text-center text-lg font-medium">
+        <hr className="my-4 text-gray-200 mt-4 sm:mt-6" />
+        <div className="mt-4 sm:mt-6 text-center text-sm sm:text-lg font-medium">
           Don&apos;t have an account?{" "}
-          <Link href="/signup" className="hover:underline text-vetarent-blue text-lg font-semibold">
+          <Link href="/signup" className="hover:underline text-vetarent-blue text-sm sm:text-lg font-semibold">
             Signup
           </Link>
         </div>
